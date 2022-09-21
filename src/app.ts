888b4dd552f8502a645890e8d4f3baacdf43583d
Wendril-Costa <wendrild.c.n@gmail.com>
2022-09-21 17:33:17 -0300

    Criado a rota post de '/products'

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
 
+import ProductsController from './controllers/productsController';
+
+const productsController = new ProductsController();
+
 const app = express();
 
 app.use(express.json());
+
+app.post('/products', productsController.create);
 
 export default app;
