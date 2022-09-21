701e028823b5c07f80686adb02453bc9f30c8c2b
Wendril-Costa <wendrild.c.n@gmail.com>
2022-09-21 17:45:01 -0300

    Arquivo productsModel criada e configurada

---
+++ src/models/productsModel.ts
@@ -0,0 +1,21 @@
+import { Pool, ResultSetHeader } from 'mysql2/promise';
+import { Product } from '../interfaces/index';
+
+export default class ProductsModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async create(book: Product): Promise<Product> {
+    const { name, amount } = book;
+    const result = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO books (name, amount) VALUES (?, ?)',
+      [name, amount],
+    );
+    const [dataInserted] = result;
+    const { insertId } = dataInserted;
+    return { id: insertId, ...book };
+  }
+}
