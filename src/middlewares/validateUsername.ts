e0aec53e37a25703bfb34c8e9878a753d9badc3b
Wendril-Costa <wendrild.c.n@gmail.com>
2022-09-25 21:21:11 -0300

    ValidateUsername criado e configurado

---
+++ src/middlewares/validateUsername.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import User from '../interfaces/User';
+
+export default function validateUsername(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const { username } = req.body as User;
+
+  if (!username) {
+    return res.status(StatusCodes.BAD_REQUEST)
+      .json({ message: '"username" is required' });
+  }
+  if (typeof username !== 'string') {
+    return res.status(StatusCodes.UNPROCESSABLE_ENTITY)
+      .json({ message: '"username" must be a string' });
+  }
+
+  if (username.length < 2) {
+    return res.status(StatusCodes.UNPROCESSABLE_ENTITY)
+      .json({ message: '"username" length must be at least 3 characters long' });
+  }
+
+  next();
+}
