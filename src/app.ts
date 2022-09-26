5eeaa0453074e2378719cfe47c893bb41de71893
Wendril-Costa <wendrild.c.n@gmail.com>
2022-09-25 21:17:31 -0300

    ValidateAmount criado e configurado

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,15 @@ import userController from './controllers/userController';
 import orderController from './controllers/orderController';
 import loginController from './controllers/loginController';
 import validateLogin from './middlewares/validateLogin';
+import validateName from './middlewares/validateName';
+// import validateAmount from './middlewares/validateAmount';
 
 const app = express();
 
 app.use(express.json());
 
 app.post('/login', validateLogin, loginController.login);
-app.post('/products', productsController.create);
+app.post('/products', validateName, validateAmount, productsController.create);
 app.get('/products', productsController.getAll);
 
 app.post('/users', userController.create);
---
+++ src/middlewares/validateAmount.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import Product from '../interfaces/Product';
+
+export default function validateAmount(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const { amount } = req.body as Product;
+
+  if (!amount) {
+    return res.status(StatusCodes.BAD_REQUEST)
+      .json({ message: '"amount" is required' });
+  }
+  if (typeof amount !== 'string') {
+    return res.status(StatusCodes.UNPROCESSABLE_ENTITY)
+      .json({ message: '"amount" must be a string' });
+  }
+
+  if (amount.length < 2) {
+    return res.status(StatusCodes.UNPROCESSABLE_ENTITY)
+      .json({ message: '"amount" length must be at least 3 characters long' });
+  }
+
+  next();
+}
