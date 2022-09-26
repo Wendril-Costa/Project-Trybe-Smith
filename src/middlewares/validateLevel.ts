079078f21ccb81e1761833c592f17a29b736ca30
Wendril-Costa <wendrild.c.n@gmail.com>
2022-09-25 21:24:42 -0300

    ValidateLevel criado e configurado

---
+++ src/middlewares/validateLevel.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import User from '../interfaces/User';
+
+export default function validateLevel(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  const { level } = req.body as User;
+
+  if (!level) {
+    return res.status(StatusCodes.BAD_REQUEST)
+      .json({ message: '"level" is required' });
+  }
+  if (typeof level !== 'number') {
+    return res.status(StatusCodes.UNPROCESSABLE_ENTITY)
+      .json({ message: '"level" must be a number' });
+  }
+
+  if (level < 0) {
+    return res.status(StatusCodes.UNPROCESSABLE_ENTITY)
+      .json({ message: '"level" must be greater than or equal to 1' });
+  }
+
+  next();
+}
